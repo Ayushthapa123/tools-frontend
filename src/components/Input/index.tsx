edcdbf1a8c7bd7c452770bf046f289b2909abb55
sagarEGme <sagaregmi0710@gmail.com>
2025-06-22 22:10:46 +0545

    fix create hostel modal and components fix

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -23,7 +23,7 @@ const Input = forwardRef<HTMLInputElement, InputComponentProps>(
     return (
       <div className={`flex flex-col space-y-1 ${className}`}>
         {label && (
-          <label htmlFor={restProps.name} className="text-sm font-semibold text-gray-600">
+          <label htmlFor={restProps.name} className="text-base font-semibold text-gray-600">
             {label}
             {restProps.required && <span className="text-error">*</span>}
           </label>
@@ -31,14 +31,14 @@ const Input = forwardRef<HTMLInputElement, InputComponentProps>(
         <div className="relative">
           <input
             ref={ref} // Forward the ref to the input element
-            className={`${height === 'lg' ? 'h-[3rem]' : 'h-[2.7rem]'} w-full rounded-md border border-[#B6C2E2] px-3 py-2 focus:border-indigo-500 focus:outline-none`}
+            className={`${height === 'lg' ? 'h-[4rem]' : 'h-14'} w-full rounded-xl border border-[#959eb4] px-3 py-2 focus:border-[#484beb] focus:outline-none placeholder:text-[16px]`}
             type={type === 'password' && isPasswordVisible ? 'text' : type}
             maxLength={255}
             {...restProps}
           />
           {type === 'password' && (
             <div
-              className="absolute inset-y-0 right-3 flex cursor-pointer items-center"
+              className="absolute inset-y-0 text-2xl right-3 flex cursor-pointer items-center"
               onClick={togglePasswordVisibility}
             >
               {/* This is a placeholder for the eye icon. Replace with your preferred icon component. */}
