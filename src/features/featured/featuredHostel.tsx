d6950158b10575450a5b64df05df6699406698b4
sagarEGme <sagaregmi0710@gmail.com>
2025-06-30 15:05:33 +0545

    blogs prev and next tab alignment

--- src/features/featured/featuredHostel.tsx
+++ src/features/featured/featuredHostel.tsx
@@ -21,20 +21,20 @@ export const FeaturedHostel = () => {
     return res.getAllHostels;
   };
   const { data: hostels, isLoading } = useQuery({
-    queryKey: ['getFeaturedHostels'],
+    queryKey: [ 'getFeaturedHostels' ],
     queryFn: () => getFeaturedHostels(),
   });
 
   return (
     <div className="mx-auto flex w-[90vw] flex-col items-center justify-center py-6">
-     <div className="my-6 flex flex-col items-center justify-between gap-0">
-  <h2 className="m-0 text-xl font-bold text-primary lg:text-4xl">
-    Top-Rated Hostels for Students & Travelers
-  </h2>
-  <p className="text-center text-sm lg:text-base tracking-wider">
-    Explore our handpicked selection of the best hostels offering comfort, affordability, and great amenities.
-  </p>
-</div>
+      <div className="my-6 flex flex-col items-center justify-between gap-0">
+        <h2 className="m-0 text-xl font-bold text-primary lg:text-4xl">
+          Top-Rated Hostels for Students & Travelers
+        </h2>
+        <p className="text-center text-sm lg:text-base tracking-wider">
+          Explore our handpicked selection of the best hostels offering comfort, affordability, and great amenities.
+        </p>
+      </div>
 
       {isLoading && (
         <div className="grid w-full grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
@@ -46,7 +46,7 @@ export const FeaturedHostel = () => {
 
       <div className="grid w-full grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
         {hostels?.data?.map(hostel => {
-          const mainWallpaper = hostel?.gallery?.filter(img => img.isSelected === true)[0]??hostel?.gallery?.[0];
+          const mainWallpaper = hostel?.gallery?.filter(img => img.isSelected === true)[ 0 ] ?? hostel?.gallery?.[ 0 ];
           var imgUrl = mainWallpaper?.url ?? '/images/default-image.png';
           if (imgUrl == 'https://example.com/image.jpg') imgUrl = '/images/default-image.png';
           return (
@@ -60,8 +60,8 @@ export const FeaturedHostel = () => {
                   city={hostel?.address?.city ?? ''}
                   subCity={hostel?.address?.subCity ?? ''}
                   description={hostel?.description ?? ''}
-                  amount={hostel?.rooms?.[0]?.price?.baseAmountPerDay ?? 0}
-                  currency={hostel?.rooms?.[0]?.price?.currency ?? ''}
+                  amount={hostel?.rooms?.[ 0 ]?.price?.baseAmountPerDay ?? 0}
+                  currency={hostel?.rooms?.[ 0 ]?.price?.currency ?? ''}
                   imgUrl={String(imgUrl)}
                   oneSeater={null}
                   twoSeater={null}
