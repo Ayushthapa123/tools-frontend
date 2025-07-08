145210923061ebb9b0b1513abe2368a4e0f13ac2
Ayush Thapa <thapaaayush115@gmail.com>
2025-07-08 21:52:15 +0545

    Fix: fix security in frontend

--- src/app/app/hostels/page.tsx
+++ src/app/app/hostels/page.tsx
@@ -4,8 +4,14 @@ import React, { useState } from 'react';
 // import { MapComponent } from './GoogleMap';
 import { AllHostels } from './AllHostels';
 import { MapProvider } from 'src/features/MapProvider';
-
+import { useUserStore } from 'src/store/userStore';
+import { UserType } from 'src/gql/graphql';
+import { notFound } from 'next/navigation';
 export default function Home() {
+  const {user}=useUserStore()
+  if(user.userType!==UserType.Superadmin){ 
+    return notFound()
+  }
   return (
     <div className="w-full ">
       <div className="flex w-full items-center justify-center ">
--- src/app/app/service-options/page.tsx
+++ src/app/app/service-options/page.tsx
@@ -3,8 +3,14 @@
 import React, { useState } from 'react';
 // import { MapComponent } from './GoogleMap';
 import { AllServices } from './AllServices';
-
+import { useUserStore } from 'src/store/userStore';
+import { UserType } from 'src/gql/graphql';
+import { notFound } from 'next/navigation';
 export default function Home() {
+  const {user}=useUserStore()
+  if(user.userType!==UserType.Superadmin){ 
+    return notFound()
+  }
   return (
     <div className="w-full ">
       <div className="flex w-full items-center justify-center ">
