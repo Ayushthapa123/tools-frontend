34d8035f3b891ef842b7092842b5432ecf65d635
Ayush Thapa <thapaaayush115@gmail.com>
2025-09-11 18:09:02 +0545

    fix: fix robot.ts file

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,13 +1,20 @@
-import type { MetadataRoute } from "next";
+import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
-        userAgent: "*",
-        allow: "/",
+        userAgent: '*',
+        allow: '/',
       },
     ],
-    sitemap: "https://www.toolsland.ai/sitemap.xml",
+    sitemap: [
+      'https://www.toolsland.ai/sitemap.xml',
+      'https://www.toolsland.ai/domain/sitemap.xml',
+      'https://www.toolsland.ai/ai-type/sitemap.xml',
+      "https://www.toolsland.ai/ai-capability/sitemap.xml",
+      "https://www.toolsland.ai/product-type/sitemap.xml",
+      "https://www.toolsland.ai/user-type/sitemap.xml",
+    ],
   };
 }
